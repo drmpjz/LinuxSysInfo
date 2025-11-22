f54005e2c4d957747311306218fcde698f4b07ab
Dr. Martin P.J. Zinser <martin.p.j.zinser@gmail.com>
2025-11-22 20:06:44 +0100

    Add dict import

--- compinfo.py
+++ compinfo.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
+
 class COMPinfo:
-    def __init__(self):
-        try:
-            inp = open('/sys/class/dmi/id/sys_vendor', 'r')
-        except:
-            self.vendor = "Unknown"
-        self.vendor = inp.readline().rstrip()        
-        try:
-            inp = open('/sys/class/dmi/id/product_name', 'r')
-        except:
-            self.model = "Unknown"
-        self.model = inp.readline().rstrip()        
+    def __init__(self, loadData=None):
+#
+#  Initialize data fields
+#
+        self.vendor      = "Unknown"
+        self.model       = "Unknown"
+
+        if not loadData:
+#
+# Retrieve data from local machine
+#
+            try:
+                inp = open('/sys/class/dmi/id/sys_vendor', 'r')
+                self.vendor = inp.readline().rstrip()        
+            except:
+                pass
+            try:
+                inp = open('/sys/class/dmi/id/product_name', 'r')
+                self.model = inp.readline().rstrip()        
+            except:
+                pass
+        else:
+#
+#  Re-create object from saved dictionary
+#
+            for key in vars(self):
+                setattr(self, key, loadData[key])
+
 
     def __str__(self):
         COMPstring = "Vendor: {0} Model: {1}. ".format(
@@ -20,3 +38,9 @@ class COMPinfo:
 if __name__ == '__main__':
     MyCOMP = COMPinfo()
     print(MyCOMP)
+    if False:
+        compDict = dict(vendor="DEC", model="PDP-11")
+        loadComp = COMPinfo(compDict)
+        print(loadComp)
+
+
